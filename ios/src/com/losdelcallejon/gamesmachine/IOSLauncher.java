a805cee1a454474379d30ee13571bab85f11ce65
Luifer <luifer.2341@gmail.com>
2016-12-09 14:57:33 -0400

    Initial Commit

---
+++ ios/src/com/losdelcallejon/gamesmachine/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.losdelcallejon.gamesmachine;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.losdelcallejon.gamesmachine.AbcGameMain;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new AbcGameMain(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
